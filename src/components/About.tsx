efe364906706fe02de48bd32cc1164d51f700c8e
geraldinas <geraldinagalvarez@gmail.com>
2024-11-21 15:32:24 -0500

    updated About Me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,23 +4,17 @@ import Socials from './Socials.tsx'
 
 export default function About({arrowIcon, timelineRef}) {
     function handleScroll() {
-        window.scroll({
-          top: document.body.offsetHeight,
-          left: 0, 
-          behavior: 'smooth',
+        timelineRef.current.scrollIntoView({ 
+            behavior: 'smooth'
         });
     }
 
     const aboutMe = (
         <div className="font-mono flex flex-col items-center justify-center text-zinc-50 p-10 pb-20">
             <p className=" cursor-pointer text-sm md:hover:scale-125 md:transition ease-in-out md:max-w-6xl md:text-lg">
-              Hello! I’m a Software Engineer currently based in NYC, with a unique background that bridges the worlds of art and technology. My journey began with a deep passion for music and large-scale painting, where I learned the power of creativity and expression on a grand scale. This artistic foundation naturally evolved into a fascination with technology, sparked during my college years through exploratory projects with Arduino and AI.
+              Hello! I'm a Software Engineer based in NYC, driven by a passion for education, mentorship, and bridging art with technology. My journey with programing began exploring Arduino, AI, and later web development, through self-learning and training at the Flatiron School. I'm dedicated to building thoughtful, efficient, and accessible solutions.
               <br></br>
-              <br></br>
-              Over time, this curiosity transformed into a love for web development, fueled by self-learning and formal training at the Flatiron School. I’m particularly passionate about integrating art into tech, creating solutions that not only function efficiently but also resonate on a deeper, more human level. I believe in a harmonious approach to technology—one that enhances our connection to each other and the world around us.
-              <br></br>
-              <br></br>
-              Beyond coding, I’m deeply committed to mentorship, fostering diversity in tech spaces, and advocating for accessibility. I also have a strong appreciation for beautifully written documentation, understanding that clarity and elegance in code can transform how we communicate and collaborate. My mission is to contribute to projects that expand our potential while keeping the human touch at the forefront.
+              I care deeply about fostering diversity in tech and creating beautifully clear documentation to enhance collaboration. My mission is to contribute to projects that connect people while keeping the human touch at the forefront. Outside of tech, I have a deep appreciation for large-scale painting and creative expression.
             </p>
         </div>
     );
